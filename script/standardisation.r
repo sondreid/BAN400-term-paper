4dbc73ff9e9d5ba2796aa2ee2cd959184f568240
Sondre Eide <sondre.eide@gmail.com>
2020-10-09 15:25:22 +0200

    Standarisation

--- script/standardisation.r
+++ script/standardisation.r
@@ -9,18 +9,43 @@ library(lubridate)
 library(janitor)  
 
 
+
+standardiseCountry <- function(country) {
+  
+}
+
 getFormat <- function(country) {
   country <- tolower(country)
   path <- paste("../datasett/", country, "/", country, "_format.xlsx")
   path <- path %>% gsub(" ", "",.)
-  print(path)
   return (path)
 }
 
 getStandard <- function() {return("../datasett/Datastandard/data_standard.xlsx") }
 
+
+"Flyttes trolig"
+"Change to allow for multiple datasets, (such as the case with France"
+getCountry <- function(country) {
+  country <- tolower(country)
+  path <- paste("../datasett/", country, "/", country, "_data.xlsx")
+  path <- path %>% gsub(" ", "",.)
+  return (path)
+}
+
+"Ikke ferdig"
 genderFunc <- function(country) {
-  countryFormat <- read_excel(getFormat(country)) %>% select(Gender)
-  standard <- read_excel(getStandard()) %>% select(Gender)
+  
+  countryFormat <- read_excel(getFormat(country))
+  standardMale <- read_excel(getStandard()) %>% select(Gender)
+  country <- getCountry(country)
+  genderCol <- colnames(country)[countryFormat[[2]][1]]
+  country %>% 
+    mutate(genderCol = lapply(standardiseGender()))
   print(standard)
+}
+
+standardiseGender <- function(gender, standardGender) {
+  
+  
 }
