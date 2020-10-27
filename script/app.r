9024da6fa4846cdbc9e0e824ecbf1e22f286ef29
Olai <olaiviken@icloud.com>
2020-10-27 16:10:07 +0100

    Basic shiny

--- script/app.r
+++ script/app.r
@@ -1,6 +1,18 @@
 library(shiny)
 
-source("analysis.r")
+ui <- fluidPage(
+  sliderInput(inputId = "num",
+              label = "Choose a number",
+              value = 25, min = 1, max = 100),
+  checkboxGroupInput(inputId = "check",
+                     label = "Checkbox Group",
+                     choiceNames = c("True", "False", "Both"),
+                     )
+  #submitButton(inputId = "act",
+   #            label = "Remove")
+)
 
+server <- function(input, output){}
 
+shinyApp(ui = ui, server = server)
 
