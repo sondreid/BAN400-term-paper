e33ad85be842ad2d57a525ee53bfe2ae3d181e0a
Sondre Eide <sondre.eide@gmail.com>
2020-12-01 17:42:29 +0100

    Start på ML

--- script/ML.r
+++ script/ML.r
@@ -19,6 +19,8 @@ load("../datasett/processed_data_all_countries.Rda")
 "load totaldata"
 load("Shiny/data/totaldata.Rda")
 
+
+#### Data selection --------------------------------------------
 aggregateAgegroup <- function() {
   totaldata %<>% 
     transform(week = as.factor(week),
@@ -41,38 +43,73 @@ totaldata %<>%
 intrain <- createDataPartition(y = totaldata$excess_deaths,
                                     p = 0.8,
                                     list = FALSE)
-
+# Splitting data ---------------------------------------------
 
 training_data <- totaldata[intrain,] #%>% select(excess_deaths,week)
 test_data     <- totaldata[-intrain,]
 
-.f <- function() {
-  gbmModel <- train(excess_deaths ~.,
-                  distribution = "bernoulli",
-                  data = training_data,
-                  n.trees = 500,
-                  interaction.depth = 4,
-                  shrinkage = 0.01)
-
-}
 
 
-## Make a model-----------------------------------------
 
+## Model creation -----------------------------------------
+
+#Make clusters
+cl <- makePSOCKcluster(detectCores())
+registerDoParallel(cl)
+
+#
 linnearreg <- train(excess_deaths ~ week,
                  data = training_data,
                  method = "lm")
 
-cl <- makePSOCKcluster(detectCores())
-registerDoParallel(cl)
+
+control <- trainControl(method = "repeatedcv",
+                 n = 10,
+                 repeats = 5)
+
+
 RFmodel <- train(excess_deaths ~.,
-                   data = training_data,
-                   method = "rf")
+                 data = training_data,
+                 method = "rf",
+                 metric = "RMSE",
+                 trcontrol = control)
+
+
+
+
+#Print relevant statistics
 print(linnearreg)
 summary(linnearreg)
 print(RFmodel)
 
+stopCluster(cl) #Stop cluster 
 
-### Prediction #--------------------------
+### Prediction #---------------------------------------------
 predict(linearreg, test_data)
-predict(RFmodel, test_data)
+
+rfPrediction <- predict(RFmodel, newdata = test_data)
+ 
+postResample(pred = rfPrediction, obs = test_data$excess_deaths) #Evaluate
+
+
+get_predicted_score <- function(model = RFmodel, week,gender,agegroup,deaths){
+    
+    df <- 
+      data.frame(
+        week = as.factor(week),
+        gender = as.factor(gender),
+        agegroup = as.factor(agegroup),
+        deaths = as.numeric(deaths)
+      )
+    
+    
+    df <- totaldata
+    
+    data.frame(
+      scoring_date = as.Date(Sys.Date()),
+      prob = 
+        predict(
+          model, 
+          newdat = df,
+          type = "prob")$Default)
+  }
